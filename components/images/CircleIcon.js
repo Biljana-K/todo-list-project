b253e9a4b3aef338f42816b171014091e28d54f8
Biljana Krstajic <biljana.web.development@gmail.com>
2024-07-08 10:19:22 +0200

    updated todo icons

--- components/images/CircleIcon.js
+++ components/images/CircleIcon.js
@@ -3,19 +3,31 @@ const CircleButton = () => {
     <svg
       style={{ width: "1.5rem" }}
       xmlns="http://www.w3.org/2000/svg"
-      fill="transparent"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="size-6"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-circle"
+      viewBox="0 0 16 16"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-      />
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
     </svg>
   );
 };
 
 export default CircleButton;
+
+export const DoneButton = () => {
+  return (
+    <svg
+      style={{ width: "2rem" }}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-check2"
+      viewBox="0 0 16 16"
+    >
+      <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
+    </svg>
+  );
+};
