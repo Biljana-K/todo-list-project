c4a0827127801c179067b8b8af24c58c6fd983b1
Biljana Krstajic <biljana.web.development@gmail.com>
2024-07-08 07:54:50 +0200

    deleted unnecessary classes import

--- components/layout/Card.js
+++ components/layout/Card.js
@@ -1,7 +1,7 @@
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  //const classes = "card" + props.className;
+  
   return (
     <>
       <div className={classes.card}>{props.children}</div>
